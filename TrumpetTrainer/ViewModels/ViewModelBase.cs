c4e6725d2ee863d5f0a87b312db661f9edc516a9
Melanchrom <melanchrom@gmail.com>
2025-02-07 01:04:17 +0100

    created empty avalonia cross platform template

---
+++ TrumpetTrainer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TrumpetTrainer.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ TrumpetTrainer/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TrumpetTrainer.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
